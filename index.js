14b11e90b2c0bef808d9e599716f3813423a8ced
Nikolay <ladanov_n@mail.ru>
2018-08-08 17:26:32 +0300

    init

--- index.js
+++ index.js
@@ -2,48 +2,70 @@ const Alice = require('yandex-dialogs-sdk')
 const db = require('./db/db.json');
 const alice = new Alice()
 
-const { button } = Alice
-
-const aliceCorsMiddleware = (params) => {
-  return ctx => {
-    if (ctx.server) {
-      ctx.server.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-        next()
-      })
-    }
-  }
-}
-
-alice.use(aliceCorsMiddleware())
+const { button, reply } = Alice
 
 alice.welcome((ctx) => {
-  ctx.reply({
-    text: 'Привет! Тебя приветствует Home Bot. Чем я могу быть полезен ?',
+  const replyMsg = reply({
+    text: 'Привет! Тебя приветствует Home Bot. \n Чем я могу быть полезен ?',
     buttons: [button('Что ты умеешь ?')]
   })
+  ctx.reply(replyMsg)
 });
 
 alice.command('Что ты умеешь ?', (ctx) => {
-  ctx.reply({
-    text: 'Сейчас на нахожуть на этапе разработки, но уже сейчас я умею: ',
-    buttons: [button('Показать мои устройства'),
-              button('Включить свет в кухне'),
-              button('Включить свет в спальне'),
-              button('Выключить весь свет')]
-  });
+  const replyMsg = reply({
+    text: 'Сейчас на нахожуть на этапе разработки, но уже сейчас я могу: ',
+    buttons: [button('Показать ваши устройства'),
+              button('Включить свет')]
+  })
+  ctx.reply(replyMsg);
 });
 
 alice.command(['Показать мои устройства', 
+               'Показать ваши устройства',
                'Что у меня есть ?'], (ctx) => {
-    console.log(ctx.userId)
+
+    const user = db[0];
+
+    const Msg = {
+      text: "Ваши устройства",
+      card: {
+        type: "ItemsList",
+        header: {
+          text: "Ваши устройства"
+        },
+        items: []
+      }
+    };
+
+    for (let device of user.user_devices){
+      Msg.card.items.push({
+        image_id: device.image.id,
+        title: device.name
+      });
+    }
+
+    const replyMsg = reply(Msg);
+    ctx.reply(replyMsg)
       
   });
+
+alice.command('включи свет', ctx => {
+  const user = db[0];
+
+  for (let device of user.user_devices){
+    if (device.type === 'light') {
+      // mqtt request
+    }
+  };
+
+  ctx.reply('Рада стараться');
+})
+
 
 alice.any((ctx) => {
   ctx.reply('Увы, я пока не знакома с этой командой')
 })
 
-const port = 3030;
+const port = 8000;
 alice.listen('/', port).then(console.log('listening on: ', port))
